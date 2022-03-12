abb44a16cd6f00ae524cbeda8a351044bdacfd07
Christopher Carroll <ccarroll4812@gmail.com>
2022-03-12 14:10:37 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
-Function emacs {emacs.exe -nw}
+## This maps emacs so that it starts in PowerShell, rather than as an extrnal GUI. This is so I can have a command line text editor on Windows
+Function emacs {emacs.exe -nw $args}
 
+## I use this personally to write in my journal. The formatting is military standard and looks like 03Mar22 or some similar formatting. Day, month, year.
 Function journal {
     $date = Get-Date -Uformat %d%b%y
     emacs.exe -nw --visit "~/../../Lab/demo-notebook/journal/$date.org"
